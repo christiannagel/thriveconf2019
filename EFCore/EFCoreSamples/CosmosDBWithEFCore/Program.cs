a167e61d55bf6f1db7c15ef91310bbd96772ed2d
Christian Nagel <christian@christiannagel.com>
2019-11-11 21:55:59 +0100

    using host class

--- EFCore/EFCoreSamples/CosmosDBWithEFCore/Program.cs
+++ EFCore/EFCoreSamples/CosmosDBWithEFCore/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
@@ -15,8 +16,32 @@ namespace CosmosDBWithEFCore
         {
             try
             {
-                ConfigureServices(args);
-                var service = Container.GetRequiredService<BooksService>();
+                using var host = Host
+                    .CreateDefaultBuilder(args)
+                    .ConfigureAppConfiguration(configuration =>
+                    {
+#if DEBUG
+                        configuration.AddUserSecrets("1E2D66CC-11C9-4DE7-B25E-F1EAA5F0154A");
+#endif
+                    })
+                    .ConfigureServices((context, services) =>
+                    {
+                        IConfigurationSection configSection = context.Configuration.GetSection("CosmosSettings");
+
+                        services.AddDbContext<BooksContext>(
+                            options => options.UseCosmos(
+                            configSection["ServiceEndpoint"],
+                            configSection["AuthKey"],
+                            configSection["DatabaseName"]));
+
+                        services.AddTransient<BooksService>();
+
+                        services.AddLogging(options =>
+                            options.AddDebug().SetMinimumLevel(LogLevel.Trace));
+
+                    }).Build();
+
+                var service = host.Services.GetRequiredService<BooksService>();
                 await service.CreateTheDatabaseAsync();
                 await service.WriteBooksAsync();
                 service.ReadBooks();
@@ -27,38 +52,5 @@ namespace CosmosDBWithEFCore
                 Console.WriteLine(ex.Message);
             }
         }
-
-        public static void ConfigureServices(string[] args)
-        {
-            var configurationBuilder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables()
-                .AddCommandLine(args);
-
-#if DEBUG
-            configurationBuilder.AddUserSecrets("1E2D66CC-11C9-4DE7-B25E-F1EAA5F0154A");
-#endif
-            IConfigurationRoot config = configurationBuilder.Build();
-
-            IConfigurationSection configSection = config.GetSection("CosmosSettings");
-
-            var services = new ServiceCollection();
-
-            services.AddDbContext<BooksContext>(
-                options => options.UseCosmos(
-                    configSection["ServiceEndpoint"], 
-                    configSection["AuthKey"], 
-                    configSection["DatabaseName"]));
-
-            services.AddTransient<BooksService>();
-
-            services.AddLogging(options =>
-                options.AddDebug().SetMinimumLevel(LogLevel.Trace));
-
-            Container = services.BuildServiceProvider();
-        }
-
-        public static ServiceProvider Container { get; private set; } = default!;
     }
 }
